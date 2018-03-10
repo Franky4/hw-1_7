633109542284075e0bddc5018e8f02bd7ddaca10
Aleksei Kozlov <mail@reg65.com>
2018-03-10 13:27:53 +1100

    расширен функционал животных

--- animals.py
+++ animals.py
@@ -1,54 +1,106 @@
 
 
-class animals:
-    paws = None
+class animals:  #  общий класс животные
+    paws = None  # лапы
+    eyes = 2     # глазка
+    life = None  # уровень здоровья
 
-    def speaks(self):
-        sound = None
+    def speaks(self):   # издает звуки
+        None
 
- class birds(animals):
+    def eat(self):      # кушает
+        None
+
+    def go(self):  # ходит
+        None
+
+    def give(self): # сколько продукции дает животное (молоко, яйца)
+        None
+
+
+class birds(animals): # Класс птицы
      wings = 2
+     paws = 2
+     life = 100 # %
 
+     def eat(self,food):
+         if food == 'пшено' or food=='вода':
+             if self.life<100:
+                 self.life+=1
+                 print('Кушаем. Уровень здоровья - {}'.format(self.life))
 
-class cow(animals):
+     def go(self):
+         self.life-=1
+         print('Гуляем. Уровень здоровья - {}'.format(self.life))
+
+     def give(self):
+         print('еще одно новое яйцо')
+
+class mammal(animals): # Класс млекопитающие
     paws = 4
+    life = 100
+
+    def eat(self,food):
+        if food == 'травка' or food=='вода':
+            if self.life<100:
+                self.life+=1
+                print('Уровень здоровья - {}'.format(self.life))
+    def go(self):
+        self.life-=2
+        print('Уровень здоровья - {}'.format(self.life))
+    def give(self):
+        print('свежее молоко')
+
+class cow(mammal):
 
     def speaks(self):
         print('Мууууу')
 
-class goat(animals):
-    paws = 4
+class goat(mammal):
 
     def speaks(self):
         print('Меееее')
 
-class sheep(animals):
-    paws = 4
+class sheep(mammal):
 
     def speaks(self):
         print('Беееее')
 
-class pig(animals):
-    paws = 4
+class pig(mammal):
 
     def speaks(self):
         print('Хрю-Хрю')
 
-class duck(animals):
-    paws = 2
+    def eat(self,food):
+        if food == 'комбикорм' or food == 'вода':
+            if self.life<100:
+                self.life+=1
+                print('Уровень здоровья - {}'.format(self.life))
+        else:
+            self.life-=1
+            print('Уровень здоровья - {}'.format(self.life))
+
+    def go(self):
+        self.life-=3
+        print('Уровень здоровья - {}'.format(self.life))
+
+    def give(self):
+        self.life=0
+        print('Свежее мясо')
+
+
+class duck(birds):
 
     def speaks(self):
         print('Кря-Кря')
 
-class chicken(animals):
-    paws = 2
+class chicken(birds):
 
     def speaks(self):
         print('Ко-ко-ко')
 
 class gees(birds):
-    paws = 2
-    
+
     def speaks(self):
         print('Га-га-га')
 
--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import animals
 
 duck1 = animals.duck()
 duck1.speaks()
+
+for i in range(5):
+    duck1.go()
+
+for i in range(5):
+    duck1.eat('пшено')
+
+duck1.give()
