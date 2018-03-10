4b7f920ff76de4b130590d1a4f80ad0a67976279
Aleksei Kozlov <mail@reg65.com>
2018-03-10 11:49:19 +1100

    новые классы

---
+++ animals.py
@@ -0,0 +1,37 @@
+
+
+class animals:
+    paws = None
+
+    def speaks(self):
+        sound = None
+
+
+class cow(animals):
+    paws = 4
+
+    def speaks(self):
+        print('Мууууу')
+
+class goat(animals):
+    paws = 4
+
+    def speaks(self):
+        print('Меееее')
+
+class sheep(animals):
+    paws = 4
+
+    def speaks(self):
+        print('Беееее')
+
+class pig(animals):
+    paws = 4
+
+    def speaks(self):
+        print('Хрю-Хрю')
+
+
+
+
+
